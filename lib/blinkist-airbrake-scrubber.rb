82ca47f6587edb20007a3c811bb2bedbf7ba3964
Paweł Komarnicki <pawel@blinkist.com>
2018-05-24 12:14:42 +0200

    Reverted Airbrake prepending to avoid load order issues

--- lib/blinkist-airbrake-scrubber.rb
+++ lib/blinkist-airbrake-scrubber.rb
@@ -3,6 +3,14 @@ Dir[File.expand_path('../../lib/**/*.rb', __FILE__)].each do |f|
   require f
 end
 
+# Prepend the original Airbrake module
+# Note: Do not remove from this file, it needs to be before Blinkist::Airbrake::Scrubber
+module Airbrake
+  class << self
+    prepend Blinkist::Airbrake::Scrubber
+  end
+end
+
 # Set up the namespace and run every scrubber listed in SCRUBBERS
 module Blinkist
   module Airbrake
--- lib/blinkist-airbrake-scrubber/airbrake.rb
+++
@@ -1,6 +0,0 @@
-# Prepend the original Airbrake module
-module Airbrake
-  class << self
-    prepend Blinkist::Airbrake::Scrubber
-  end
-end
