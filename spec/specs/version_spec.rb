b9e4f32bce35e436389a0533f3a9eec81b38918c
John Donner <johnbdonner@gmail.com>
2020-05-25 15:27:07 -0400

    [FIX] why is this a spec

--- spec/specs/version_spec.rb
+++ spec/specs/version_spec.rb
@@ -10,8 +10,8 @@ describe Blinkist::AirbrakeScrubber::VERSION do
     expect(version.instance_of?(String)).to be true
   end
 
-  it 'equals 4.0.1 for auto-check purposes' do
+  it 'equals 4.2.0 for auto-check purposes' do
     version = Blinkist::AirbrakeScrubber::VERSION
-    expect(version).to eq '4.1.1'
+    expect(version).to eq '4.2.0'
   end
 end
